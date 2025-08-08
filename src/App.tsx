a9c027b168a3ebb43aa7f507f4b1af41e3fe998c
shion_inoue <shion.akasal@gmail.com>
2025-08-08 12:08:27 +0900

    browserRouterからhasRouterに変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./components/pages/Home";
 import { Skill } from "./components/pages/Skill";
 import { Portfolio } from "./components/pages/Portfolio";
 
 function App() {
   return (
-    <BrowserRouter
-      basename={
-        import.meta.env.DEV ? "" : "https://oshio-akami.github.io/portfolio/"
-      }
-    >
+    <HashRouter basename={import.meta.env.DEV ? "/" : "/portfolio"}>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Skill" element={<Skill />} />
+        <Route path="/skill" element={<Skill />} />
         <Route path="/portfolio" element={<Portfolio />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 

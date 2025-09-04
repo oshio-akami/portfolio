81170cd393f2f404ce280a883710ecbc0fd70dff
shion_inoue <shion.akasal@gmail.com>
2025-09-04 11:55:02 +0900

    Heroに顔のイラスト追加

--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -1,6 +1,11 @@
 export function Hero() {
   return (
-    <div className="flex gap-5 flex-col items-center justify-center w-full h-[100vh] -z-1">
+    <div className="relative flex gap-5 flex-col items-center justify-center w-full h-[100vh] -z-1">
+      <img
+        fade-in
+        src="face2.png"
+        className="absolute top-3/5 left-1/2 -translate-x-1/2 -translate-y-3/5 w-200 h-200 opacity-10"
+      ></img>
       <h1 className="text-6xl font-bold text-highlight tracking-wide text-center">
         Shion Inoue Portfolio
       </h1>
